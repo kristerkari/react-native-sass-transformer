009f74740f671be3ee1e49a22813c5df929ea13e
krister <krister@sent.at>
2018-01-10 01:45:50 +0200

    Add support for react-native 52

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 var sass = require("node-sass");
+var semver = require("semver");
 var css2rn = require("css-to-react-native-transform").default;
 
-let upstreamTransformer = null;
+var upstreamTransformer = null;
 
-try {
-  // handle RN >= 0.47
+var reactNativeVersionString = require("react-native/package.json").version;
+var reactNativeMinorVersion = semver(reactNativeVersionString).minor;
+
+if (reactNativeMinorVersion >= 52) {
+  upstreamTransformer = require("metro/src/transformer");
+} else if (reactNativeMinorVersion >= 47) {
   upstreamTransformer = require("metro-bundler/src/transformer");
-} catch (e) {
-  try {
-    // handle RN 0.46
-    upstreamTransformer = require("metro-bundler/build/transformer");
-  } catch (e) {
-    // handle RN <= 0.45
-    const oldUpstreamTransformer = require("react-native/packager/transformer");
-    upstreamTransformer = {
-      transform({ src, filename, options }) {
-        return oldUpstreamTransformer.transform(src, filename, options);
-      }
-    };
-  }
+} else if (reactNativeMinorVersion === 46) {
+  upstreamTransformer = require("metro-bundler/build/transformer");
+} else {
+  // handle RN <= 0.45
+  var oldUpstreamTransformer = require("react-native/packager/transformer");
+  upstreamTransformer = {
+    transform({ src, filename, options }) {
+      return oldUpstreamTransformer.transform(src, filename, options);
+    }
+  };
 }
 
 module.exports.transform = function(src, filename, options) {
